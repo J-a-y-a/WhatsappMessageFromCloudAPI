35b250cfe68a26276b4aeabf2205a95ee7035606
J-a-y-a <144910001+J-a-y-a@users.noreply.github.com>
2023-11-06 15:03:10 +0530

    Add project files.

---
+++ WhatsappMessageFromCloudAPI/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhatsappMessageFromCloudAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private async Task<string> SendWhatsAppMsg()
+        {
+            string responsestatus = "";
+            string mobile = "918522856452";
+            string templateName = "hello_world";
+            string Token = "EAAOHgq45dcwBO8CZCTAiZAZAPHUh8TCVzhJLfi6nyXgWEDxao3tzXcQd0ZBiVUiWDYGY6ZAn2HMB9EGoNtT4uUkBMENOh938WXtQvMTHkHF00ZB8HLuZBBQ0P2uvZBhXIVItVY25Yle7Eyon79m4mLZA1gEbJFZACxsGshuKGsKt5WhZCVPmt22YRgKSaTlyzKDWvaPKneMenTxZANvYJSdDH4UkclhgyKgZD";
+            string BaseUrl = "https://graph.facebook.com/v17.0/154702904386106/";
+            Uri baseurl = new Uri(BaseUrl);
+            var client = new RestSharp.RestClient(baseurl);
+            var request = new RestRequest("messages", Method.POST);
+            request.AddHeader("Authorization", "Bearer " + Token);
+            request.AddHeader("Content-Type", "application/json");
+            var body = "{\"messaging_product\": \"whatsapp\",\"to\": \"" + mobile + "\",\"type\": \"template\",\"template\": { \"name\": \"" + templateName + "\",\"language\":{ \"code\": \"en_US\"}}}";
+            request.AddJsonBody(body);
+            IRestResponse response = await client.ExecuteAsync(request);
+            //if(response.StatusCode.ToString() = "OK")
+            //{
+
+            //}
+            return responsestatus;
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WhatsappMessageFromCloudAPI/Controllers/WhatsappController.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhatsappMessageFromCloudAPI.Controllers
+{
+    public class WhatsappController : Controller
+    {
+        // GET: Whatsapp
+        public async Task<ActionResult> SendWhatsAppMsg()
+        {
+           
+            try
+            { 
+            string mobile = "918522856452";
+            string templateName = "hello_world";
+            string Token = "EAAOqxLgHJIYBO1G7H8UUNSlmjXRlPObSYfBrfsIxMt6FOG9YHBky5Ap7H2di7PAri5ZBEzSXG9P0cJUFrwdvZBpcLT7OZBUfUPftI5ihUnZCS7mMtNhHgxNIVwKHuJimKdnia2Dsvqz2TpzlcbEIwRFSoUiJxIfYaxWN8Vwc9LYv7GX0AZCnYZAXqmKr6IVPfIK1OjIByDN4d4CVleJWPQP92WxikZD";
+            string BaseUrl = "https://graph.facebook.com/v17.0/154702904386106/";
+            Uri baseurl = new Uri(BaseUrl);
+            var client = new RestSharp.RestClient(baseurl);
+            var request = new RestRequest("messages", Method.POST);
+            request.AddHeader("Authorization", "Bearer " + Token);
+            request.AddHeader("Content-Type", "application/json");
+            var body = "{\"messaging_product\": \"whatsapp\",\"to\": \"" + mobile + "\",\"type\": \"template\",\"template\": { \"name\": \"" + templateName + "\",\"language\":{ \"code\": \"en_US\"}}}";
+            request.AddJsonBody(body);
+            IRestResponse response = await client.ExecuteAsync(request);
+                if (response.IsSuccessful)
+                {
+
+                    TempData["SuccessMessage"] = "WhatsApp message sent successfully.";
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Error sending WhatsApp message: " + response.ErrorMessage;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "An error occurred: " + ex.Message;
+            }
+            return View();
+        }
+    }
+}
